5dc754eb1172d53937df63179660c8a9e6b5c548
Alexsandro Ricardo <alexsandro.sousa12366@gmail.com>
2021-04-16 17:06:54 -0300

    multer + alterando nome do file

---
+++ src/routes/fotoRoutes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import FotoController from '../controllers/FotoController';
+import multerConfig from '../config/multerCongfig';
+
+const upload = multer(multerConfig);
+
+const router = new Router();
+
+router.post('/', upload.single('foto'), FotoController.store);
+
+export default router;
